62716aded83deb8302d4c827a0cce7bae583fc01
Reidrick Barbosa <reidrick.barbosa@gmail.com.br>
2023-11-30 17:58:09 -0300

    Projeto hora do desafio

---
+++ app0.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio.';
+
+function verificarConsole() {
+    console.log('O botão foi clicado.')
+};
+
+function verificarAlert() {
+    window.alert ('Eu amo programar')
+};
